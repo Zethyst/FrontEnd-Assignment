40a28ee2a06221dc91141531a07174e4ad55988d
Ekansh <zethyst@protonmail.com>
2024-12-16 14:33:31 +0530

    Removed line-clamp module

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,35 +12,7 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/line-clamp'),
-    plugin(function ({ addUtilities }) {
-      addUtilities({
-        '.line-clamp-7': {
-          display: '-webkit-box',
-          '-webkit-line-clamp': '7',
-          '-webkit-box-orient': 'vertical',
-          overflow: 'hidden',
-        },
-        '.line-clamp-8': {
-          display: '-webkit-box',
-          '-webkit-line-clamp': '8',
-          '-webkit-box-orient': 'vertical',
-          overflow: 'hidden',
-        },
-        '.line-clamp-9': {
-          display: '-webkit-box',
-          '-webkit-line-clamp': '9',
-          '-webkit-box-orient': 'vertical',
-          overflow: 'hidden',
-        },
-        '.line-clamp-10': {
-          display: '-webkit-box',
-          '-webkit-line-clamp': '10',
-          '-webkit-box-orient': 'vertical',
-          overflow: 'hidden',
-        },
-      });
-    }),
+    
   ],
 }
 
